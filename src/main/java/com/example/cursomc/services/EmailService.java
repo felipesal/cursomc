bf404526a2f4a23d45bc95f13d87ceef135a1884
Felipe Salles <prof.felipe.salles@gmail.com>
2019-12-16 02:17:19 -0200

    Configuração inicial spring security

--- src/main/java/com/example/cursomc/services/EmailService.java
+++ src/main/java/com/example/cursomc/services/EmailService.java
@@ -3,9 +3,11 @@ package com.example.cursomc.services;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
 
 import com.example.cursomc.domain.Pedido;
 
+@Service
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
